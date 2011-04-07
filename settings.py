8b7868a5651b9ece6dfd34103dcea978fa0519e4
Tyr Chen <tyr.chen@gmail.com>
2011-04-07 10:49:21 +0800

    change database location.

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ PROJECT_HOME = os.path.dirname(os.path.realpath(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'toureet_editor', # Or path to database file if using sqlite3.
+        'NAME': '/opt/bitnami/sqlite/data/toureet_editor', # Or path to database file if using sqlite3.
         'USER': '', # Not used with sqlite3.
         'PASSWORD': '', # Not used with sqlite3.
         'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
