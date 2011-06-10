3adccf280228a64d3bb797aae4f14d9b3a34f184
Tyr Chen <tyr.chen@gmail.com>
2011-06-10 17:25:55 +0800

    make a country based summary.

--- editor/urls.py
+++ editor/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
-from editor.views import translation_summary
+from editor.views import translation_summary, translation_country_summary
 
 urlpatterns = patterns('',
-  url(r'^summary/$', translation_summary, name = 'translation_summary')
+  url(r'^summary/$', translation_summary, name = 'translation_summary'),
+  url(r'^country-summary/$', translation_country_summary, name = 'translation_country_summary')
 )
 
 
--- editor/views.py
+++ editor/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from editor.models import Country, Administration, City, Place
+from itertools import izip
 
 
 def _get_total_translations(user):
@@ -30,6 +31,39 @@ def _get_total_full_translation(user):
   places_full = cursor.fetchone()[0]
   return countries_full + regions_full + cities_full + places_full
 
+def _get_country_translation(country, size):
+  from django.db import connection, transaction
+  cursor = connection.cursor()
+  cursor.execute('select count(*) from editor_country where length(description_zh) > %s and id=%s', [size, country.id])
+  countries_full = cursor.fetchone()[0]
+  cursor.execute('select count(*) from editor_city where length(description_zh) > %s and country_id=%s', [size, country.id])
+  cities_full = cursor.fetchone()[0]
+  cursor.execute('select count(*) from editor_place where length(description_zh) > %s and country_id=%s', [size, country.id])
+  places_full = cursor.fetchone()[0]
+  all = countries_full + cities_full + places_full
+  return [countries_full, cities_full, places_full, all]
+
+
+@login_required
+def translation_country_summary(request):
+  result = {}
+  h_total = [0, 0, 0, 0, 0, 0]
+  for country in Country.objects.all():
+    if country.added_by:
+      total = _get_country_translation(country, 0)
+      all = total[3]
+      if all:
+        full = _get_country_translation(country, 150)[3]
+        total.append(full)
+        total.append(all - full)
+        result["%s (%s)" % (country.name_en, country.added_by.get_full_name())] = total
+        h_total = map(sum, izip(h_total, total))
+
+  variables = RequestContext(request, {
+    'data': result,
+    'total': h_total,
+  })
+  return render_to_response('translation_country_summary.html', variables)
 
 @login_required
 def translation_summary(request):
